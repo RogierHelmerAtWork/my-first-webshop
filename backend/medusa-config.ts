526929aa85e8b8766893a708ba21b2746fcff9a1
rogier <rogierhelmer@gmail.com>
2026-03-10 23:00:21 +0100

    Fix Cors

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -10,9 +10,10 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || backendUrl,
+      adminCors: process.env.ADMIN_CORS || backendUrl,
+      authCors: process.env.AUTH_CORS || backendUrl,
+      
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
